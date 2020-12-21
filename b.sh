94fffb102e527d7c72270e3c66066f4ddce03291
Thaison Nguyen <thieson08@me.com>
2020-12-21 16:39:05 +0100

    remove b.sh

--- b.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SUFFIX=""
-VERSION=$(grep "AC_INIT" configure.in|awk -F, '{ print $2 }'|awk -F\) '{ print $1 }'|sed 's/ //g')
-
-autoreconf -f && ./configure --enable-udev=/lib/udev/rules.d && make && make dist && cp gnome15-${VERSION}.tar.gz ~/Workspaces/home\:tanktarta\:gnome15${SUFFIX}/gnome15
